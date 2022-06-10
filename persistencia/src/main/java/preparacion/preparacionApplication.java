19e43359fa4b4e19edd54161cab8ae3bbe77a54d
Andres Mejia <andress.mejiaf@uqvirtual.edu.co>
2022-06-10 18:58:35 -0500

    Inicio proyecto, configuracion Gradle

---
+++ persistencia/src/main/java/preparacion/preparacionApplication.java
@@ -0,0 +1,13 @@
+package preparacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class preparacionApplication {
+
+    public static void main (String [] args){
+        SpringApplication.run(preparacionApplication.class, args);
+
+    }
+}
---
+++ src/main/java/preparacion/cursoApplication.java
@@ -0,0 +1,8 @@
+package preparacion;
+
+
+public class cursoApplication {
+
+    public static void main (String[] args){
+    }
+}
